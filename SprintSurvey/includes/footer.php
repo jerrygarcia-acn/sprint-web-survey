e420c31adcfee4c33f890335934686c3a63aa7e3
Alan Gustavo Valdez Cascajares <a.valdez@accenture.com>
2018-02-08 12:59:00 -0600

    Finished working on footer resposiveness.

--- SprintSurvey/includes/footer.php
+++ SprintSurvey/includes/footer.php
@@ -2,29 +2,57 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6 col-6">
-                <ul>
-                    <li>
-                        <a href="#">News</a>
-                    </li>
-                    <li>
-                        <a href="#">About Us</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact Us</a>
-                    </li>
-                    <li>
-                        <a href="#">Legal</a>
-                    </li>
-                    <li>
-                        <a href="#">Privacy</a>
-                    </li>
-                    <li>
-                        <a href="#">Ad Choices</a>
-                    </li>
+                <ul id='mobile-footer-links' class="d-block d-md-none">
+                    <div class="row">
+                        <div class="col-md-6 col-6 m-0">
+                            <li><a href="#">News</a></li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </div>
+                        <div class="col-md-6 col-6 m-0">
+                            <li><a href="#">Legal</a></li>
+                            <li><a href="#">Privacy</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                        </div>
+                    </div>
+                </ul>
+                <ul id='footer-links' class="d-none d-md-inline-block">
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Legal</a></li>
+                    <li><a href="#">Privacy</a></li>
+                    <li><a href="#">Ad Choices</a></li>
                 </ul>
             </div>
             <div class="col-md-6 col-6">
-                <ul class="social-links float-md-right">
+                <ul class="d-block d-md-none float-md-right" id="mobile-social-links">
+                    <li>
+                        <a href="https://www.facebook.com/sprint" target="_blank"
+                           class="btn btn-social-icon btn-facebook">
+                            <span class="fa fa-facebook"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/sprintbusiness" target="_blank"
+                           class="btn btn-social-icon btn-twitter">
+                            <span class="fa fa-twitter"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/company/sprint" target="_blank"
+                           class="btn btn-social-icon btn-linkedin">
+                            <span class="fa fa-linkedin"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://plus.google.com/u/0/+Sprint/posts" target="_blank"
+                           class="btn btn-social-icon btn-google">
+                            <span class="fa fa-google"></span>
+                        </a>
+                    </li>
+                </ul>
+                <ul class="d-none d-md-inline-block float-md-right" id="social-links ">
                     <li>
                         <a href="https://www.facebook.com/sprint" target="_blank"
                            class="btn btn-social-icon btn-facebook">
@@ -53,7 +81,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 p-0">
                 <p class="copyright">&copy; <?php echo date('Y'); ?> Sprint.com. All rights reserved.</p>
             </div>
         </div>
