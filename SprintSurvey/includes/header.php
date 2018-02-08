c5d7029d5c6aca68dfdfb727d44218d2a66e5422
Alan Gustavo Valdez Cascajares <a.valdez@accenture.com>
2018-02-08 10:20:56 -0600

    Finished header responsive view on all devices, including dropdown feature.

--- SprintSurvey/includes/header.php
+++ SprintSurvey/includes/header.php
@@ -1,16 +1,20 @@
 <header>
-    <div class="container">
-        <nav class='navbar navbar-expand-md'>
-            <a href="index.php" class="navbar-brand d-inline-block d-md-none">
-                <img src="./images/SprintLogoAlone.png">
+    <div class="container p-0">
+        <nav class='navbar navbar-expand-md py-1 px-0'>
+            <a href="index.php" class="navbar-brand d-inline-block d-md-none p-0">
+                <img src="./images/SprintLogoMobile.png">
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            <p class="h6 text-sm-center d-inline-block d-md-none mx-auto">Works for Business <sup>™</sup></p>
+            <button class="navbar-toggler py-1 px-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30" height="20" focusable="false">
+                    <title>Menu</title>
+                    <path stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" d="M4 7h22M4 15h22M4 23h22"></path>
+                </svg>
             </button>
             <div class="collapse navbar-collapse row" id="navbarSupportedContent">
                 <div class="col-md-12 d-none d-md-inline-block">
-                    <ul id="top-links" class="p-0">
+                    <ul id="top-links" class="p-0 m-0 float-md-right">
                         <li><a href="">Mobility-as-a-service</a></li>
                         <li><a href="">Small Business </a>
                         </li>
@@ -24,18 +28,19 @@
                 </div>
                 <div class="col-md-12 d-none d-md-inline-block">
                     <div class="row">
-                        <div class="col-md-3 p-0">
-                            <img src="./images/SprintLogo.png">
+                        <div class="col-md-3">
+                            <a href="index.php">
+                                <img src="./images/SprintLogo.png">
+                            </a>
                         </div>
                         <div class="col-md-9 p-0">
                             <p class="h4 text-md-right p-3 m-0">Works for Business <sup>™</sup></p>
-
                         </div>
                     </div>
                 </div>
                 <div class="col-md-12">
-                <ul class="navbar nav float-md-left">
-                    <li class="nav-item">I would like to...</li>
+                <ul class="navbar nav float-left pl-2">
+                    <li class="d-none d-md-inline-block nav-item">I would like to...</li>
                     <li class="nav-item"><a href="#" class="nav-link">Solutions</a></li>
                     <li class="nav-item"><a href="#" class="nav-link">New Ideas</a></li>
                     <li class="nav-item"><a href="#" class="nav-link">Why Sprint</a></li>
