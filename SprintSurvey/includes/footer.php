aa51851707d200913c15ba1fff58002ebf7850e4
Alan Gustavo Valdez Cascajares <a.valdez@accenture.com>
2018-02-08 10:42:28 -0600

    Fixing footer responsive issues.

--- SprintSurvey/includes/footer.php
+++ SprintSurvey/includes/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="container">
         <div class="row">
-            <div class="col-md-6 col-sm-6">
+            <div class="col-md-6 col-6">
                 <ul>
                     <li>
                         <a href="#">News</a>
@@ -22,39 +22,39 @@
                         <a href="#">Ad Choices</a>
                     </li>
                 </ul>
-                <div class="col-md-6 col-sm-6">
-                    <ul class="social-links">
-                        <li>
-                            <a href="https://www.facebook.com/sprint" target="_blank"
-                               class="btn btn-social-icon btn-facebook">
-                                <span class="fa fa-facebook"></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://twitter.com/sprintbusiness" target="_blank"
-                               class="btn btn-social-icon btn-twitter">
-                                <span class="fa fa-twitter"></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.linkedin.com/company/sprint" target="_blank"
-                               class="btn btn-social-icon btn-linkedin">
-                                <span class="fa fa-linkedin"></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://plus.google.com/u/0/+Sprint/posts" target="_blank"
-                               class="btn btn-social-icon btn-google">
-                                <span class="fa fa-google"></span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+            </div>
+            <div class="col-md-6 col-6">
+                <ul class="social-links float-md-right">
+                    <li>
+                        <a href="https://www.facebook.com/sprint" target="_blank"
+                           class="btn btn-social-icon btn-facebook">
+                            <span class="fa fa-facebook"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/sprintbusiness" target="_blank"
+                           class="btn btn-social-icon btn-twitter">
+                            <span class="fa fa-twitter"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/company/sprint" target="_blank"
+                           class="btn btn-social-icon btn-linkedin">
+                            <span class="fa fa-linkedin"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://plus.google.com/u/0/+Sprint/posts" target="_blank"
+                           class="btn btn-social-icon btn-google">
+                            <span class="fa fa-google"></span>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
         <div class="row">
             <div class="col-12">
-                <p class="copyright">&copy; 2018 Sprint.com. All rights reserved.</p>
+                <p class="copyright">&copy; <?php echo date('Y'); ?> Sprint.com. All rights reserved.</p>
             </div>
         </div>
     </div>
